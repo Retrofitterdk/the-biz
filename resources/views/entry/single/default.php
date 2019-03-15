09a58b3c5a52b0dedcc1b325a2907295e6f8daba
sbangnielsen <sbn@dagensmedicin.dk>
2019-03-15 14:01:27 +0100

    Update default.php

--- resources/views/entry/single/default.php
+++ resources/views/entry/single/default.php
@@ -3,10 +3,14 @@
 	<header class="entry__header">
 		<?php Hybrid\Post\display_title() ?>
 	</header>
-
+	<div class="entry_media">
+		<?php Hybrid\View\display( 'media', Hybrid\Post\hierarchy() ) ?>
+	</div>
 	<div class="entry__content">
 		<?php the_content() ?>
-		<?php Hybrid\View\display( 'nav/pagination', 'post' ) ?>
 	</div>
+	<footer class="entry__footer">
+		<?php Hybrid\View\display( 'meta/entryfooter', Hybrid\Post\hierarchy() ) ?>
+	</footer>
 
 </article>
