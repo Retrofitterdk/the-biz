72dc3c138fd86f1cb197c77d7de6950acf77498c
sbangnielsen <sbn@dagensmedicin.dk>
2019-03-15 14:01:36 +0100

    Create feature.php

---
+++ resources/views/entry/single/feature.php
@@ -0,0 +1,19 @@
+<article <?php Hybrid\Attr\display( 'entry' ) ?>>
+
+	<header class="entry__header">
+		<?php Hybrid\Post\display_title() ?>
+	</header>
+	<div class="entry__summary">
+		<?php the_excerpt() ?>
+	</div>
+	<div class="entry_media">
+		<?php Hybrid\View\display( 'media', Hybrid\Post\hierarchy() ) ?>
+	</div>
+	<div class="entry__content">
+		<?php the_content() ?>
+		<?php Hybrid\View\display( 'nav/pagination', 'feature' ) ?>
+	</div>
+	<footer class="entry__footer">
+		<?php Hybrid\View\display( 'meta/entryfooter', 'feature' ) ?>
+	</footer>
+</article>
