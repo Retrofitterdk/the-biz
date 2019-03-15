52984c9a8aa282409d557fda1308deea25a37181
sbangnielsen <sbn@dagensmedicin.dk>
2019-03-15 16:31:58 +0100

    Add meta to teams members sinle

--- resources/views/meta/entryfooter/team-member.php
+++ resources/views/meta/entryfooter/team-member.php
@@ -1,2 +1,7 @@
+<?php
+  if ( function_exists( 'finkom_team_member_meta' ) ) :
+    finkom_team_member_meta();
+  endif;
+?>
 <?php Hybrid\Post\display_terms( [ 'taxonomy' => 'team-member-category' ] ) ?>
 <?php Hybrid\Breadcrumbs\Trail::display() ?>
