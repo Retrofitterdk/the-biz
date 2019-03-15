c5dc30031b926aba4e6ebeaaafe7271c87519b15
sbangnielsen <sbn@dagensmedicin.dk>
2019-03-15 16:32:33 +0100

    Add meta conditionally to projects

--- resources/views/meta/entrymeta/project.php
+++ resources/views/meta/entrymeta/project.php
@@ -1,1 +1,5 @@
-<?php finkom_project_meta('<ul class="project-meta">','</ul>'); ?>			
+<?php
+  if ( function_exists( 'finkom_project_meta' ) ) :
+finkom_project_meta('<ul class="project-meta">','</ul>');
+  endif;
+?>			
