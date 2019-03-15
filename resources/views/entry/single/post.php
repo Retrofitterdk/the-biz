80fa448865bfdc6a4068ce1e0835d3b87eea7b0f
sbangnielsen <sbn@dagensmedicin.dk>
2019-03-15 14:02:14 +0100

    Update post single view

--- resources/views/entry/single/post.php
+++ resources/views/entry/single/post.php
@@ -1,24 +1,19 @@
 <article <?php Hybrid\Attr\display( 'entry' ) ?>>
 
 	<header class="entry__header">
-		<?php Hybrid\View\display( 'media', Hybrid\Post\hierarchy() ) ?>
 		<?php Hybrid\Post\display_title() ?>
-
-		<div class="entry__byline">
-			<?php Hybrid\Post\display_author() ?>
-			<?php Hybrid\Post\display_date( [ 'before' => TheBiz\sep() ] ) ?>
-			<?php Hybrid\Post\display_comments_link( [ 'before' => TheBiz\sep() ] ) ?>
-		</div>
 	</header>
-
+	<div class="entry_media">
+		<?php Hybrid\View\display( 'media', Hybrid\Post\hierarchy() ) ?>
+	</div>
+		<div class="entry__meta">
+		<?php Hybrid\View\display( 'meta/entrymeta', 'post' ) ?>
+		</div>
 	<div class="entry__content">
 		<?php the_content() ?>
 		<?php Hybrid\View\display( 'nav/pagination', 'post' ) ?>
 	</div>
-
 	<footer class="entry__footer">
-		<?php Hybrid\Post\display_terms( [ 'taxonomy' => 'category' ] ) ?>
-		<?php Hybrid\Post\display_terms( [ 'taxonomy' => 'post_tag', 'before' => TheBiz\sep() ] ) ?>
+		<?php Hybrid\View\display( 'meta/entryfooter', 'post' ) ?>
 	</footer>
-
 </article>
