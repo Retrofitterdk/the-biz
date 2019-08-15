9b02918ca2cd5a5da0f0ac7332d0b8bbb7d65470
sbangnielsen <sbn@dagensmedicin.dk>
2019-08-15 22:06:24 +0200

    Style header

--- resources/views/header/default.php
+++ resources/views/header/default.php
@@ -10,17 +10,14 @@
 <div class="app">
 
 	<header <?php Hybrid\Attr\display( 'header', 'app-header', [ 'class' => 'app-header' ] ) ?>>
-
 		<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'the-biz' ) ?></a>
-
 		<div class="app-header__branding">
 			<?php the_custom_logo() ?>
 			<?php Hybrid\Site\display_title() ?>
 			<?php Hybrid\Site\display_description() ?>
 		</div>
-
 		<?php the_custom_header_markup() ?>
-
-		<?php Hybrid\View\display( 'nav/menu', 'primary', [ 'location' => 'primary' ] ) ?>
-
+		<div class="app-header__navigation">
+			<?php Hybrid\View\display( 'nav/menu', 'primary', [ 'location' => 'primary' ] ) ?>
+		</div>
 	</header>
--- resources/views/nav/menu/default.php
+++ resources/views/nav/menu/default.php
@@ -1,20 +1,26 @@
 <?php if ( has_nav_menu( $data->location ) ) : ?>
-
-	<nav <?php Hybrid\Attr\display( 'menu', $data->location ) ?>>
-
-		<h3 class="menu__title screen-reader-text">
+	<h3 class="menu__title">
+		<a href="#site-navigation" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Open primary menu', 'finkom' ); ?>" aria-expanded="false">
 			<?php Hybrid\Menu\display_name( $data->location ) ?>
+		</a>
+	</h3>
+	<nav <?php Hybrid\Attr\display( 'menu', $data->location, [ 'id' => 'site-navigation' ] ) ?>>
+		<h3 class="menu__title">
+			<a href="#" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Close primary menu', 'finkom' ); ?>" aria-expanded="false">
+				<?php Hybrid\Menu\display_name( $data->location ) ?>
+			</a>
 		</h3>
-
 		<?php wp_nav_menu( [
-			'theme_location' => $data->location,
-			'container'      => '',
-			'menu_id'        => '',
-			'menu_class'     => 'menu__items',
-			'items_wrap'     => '<ul class="%2$s">%3$s</ul>',
-			'item_spacing'   => 'discard'
+			'theme_location'  => $data->location,
+			'container'       => '',
+			'container_class' => 'nav-menu',
+  			'container_id'    => 'primary-menu-container',
+			'menu_id'         => 'primary-menu',
+			'menu_class'      => 'menu__items',
+			'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
+			'item_spacing'    => 'discard'
 		] ) ?>
 
 	</nav>
-
+	<a href="#" class="backdrop" tabindex="-1" aria-hidden="true" hidden></a>
 <?php endif ?>
