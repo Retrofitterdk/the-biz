8de4d84d22e2ad2fd1122f4f98f0ef628db43858
sbangnielsen <sbn@dagensmedicin.dk>
2019-08-16 12:17:19 +0200

    add span around menu title

--- resources/views/nav/menu/default.php
+++ resources/views/nav/menu/default.php
@@ -1,14 +1,13 @@
 <?php if ( has_nav_menu( $data->location ) ) : ?>
 	<h3 class="menu__title">
 		<a href="#site-navigation" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Open primary menu', 'finkom' ); ?>" aria-expanded="false">
-			<?php Hybrid\Menu\display_name( $data->location ) ?>
+			<span><?php Hybrid\Menu\display_name( $data->location ) ?><span>
 		</a>
 	</h3>
 	<nav <?php Hybrid\Attr\display( 'menu', $data->location, [ 'id' => 'site-navigation' ] ) ?>>
 		<h3 class="menu__title">
-			<a href="#" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Close primary menu', 'finkom' ); ?>" aria-expanded="false">
-				<?php Hybrid\Menu\display_name( $data->location ) ?>
-			</a>
+			<a href="#" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Close primary menu', 'finkom' ); ?>" aria-expanded="false"></a>
+			<?php Hybrid\Menu\display_name( $data->location ) ?>
 		</h3>
 		<?php wp_nav_menu( [
 			'theme_location'  => $data->location,
