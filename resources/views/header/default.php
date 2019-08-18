00bc722473087ee46f48b1aceee7103cfcddde23
sbangnielsen <sbn@dagensmedicin.dk>
2019-08-18 16:16:06 +0200

    Move menu toggle buttons to header template

--- resources/views/header/default.php
+++ resources/views/header/default.php
@@ -10,14 +10,26 @@
 <div class="app">
 
 	<header <?php Hybrid\Attr\display( 'header', 'app-header', [ 'class' => 'app-header' ] ) ?>>
-		<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'the-biz' ) ?></a>
-		<div class="app-header__branding">
-			<?php the_custom_logo() ?>
-			<?php Hybrid\Site\display_title() ?>
-			<?php Hybrid\Site\display_description() ?>
-		</div>
-		<?php the_custom_header_markup() ?>
-		<div class="app-header__navigation">
-			<?php Hybrid\View\display( 'nav/menu', 'primary', [ 'location' => 'primary' ] ) ?>
+		<div class="app-header__wrapper">
+			<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'the-biz' ) ?></a>
+			<div class="app-header__branding">
+				<?php the_custom_logo() ?>
+				<?php Hybrid\Site\display_title() ?>
+				<?php Hybrid\Site\display_description() ?>
+			</div>
+			<?php the_custom_header_markup() ?>
+			<div id="primary-menu-container" class="app-header__navigation">
+				<?php Hybrid\View\display( 'nav/menu', 'featured', [ 'location' => 'featured' ] ) ?>
+				<h3 class="app-header__toggle app-header__toggle--open">
+					<a href="#site-navigation" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Open primary menu', 'the-biz' ); ?>" aria-expanded="false">
+						<span class="app-header__menu-name"><?php Hybrid\Menu\display_name( 'primary' ) ?></span>
+					</a>
+				</h3>
+				<h3 class="app-header__toggle app-header__toggle--closed">
+					<span class="app-header__menu-name"><?php Hybrid\Menu\display_name( 'primary' ) ?></span>
+					<a href="#" role="button" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Close primary menu', 'the-biz' ); ?>" aria-expanded="false"></a>
+				</h3>
+				<?php Hybrid\View\display( 'nav/menu', 'primary', [ 'location' => 'primary' ] ) ?>
+			</div>
 		</div>
 	</header>
--- resources/views/nav/menu/default.php
+++ resources/views/nav/menu/default.php
@@ -1,16 +1,9 @@
 <?php if ( has_nav_menu( $data->location ) ) : ?>
-	<h3 class="menu__title">
-		<a href="#site-navigation" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Open primary menu', 'finkom' ); ?>" aria-expanded="false">
-			<span><?php Hybrid\Menu\display_name( $data->location ) ?><span>
-		</a>
-	</h3>
 	<nav <?php Hybrid\Attr\display( 'menu', $data->location, [ 'id' => 'site-navigation' ] ) ?>>
-		<h3 class="menu__title">
-			<a href="#" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Close primary menu', 'finkom' ); ?>" aria-expanded="false"></a>
-			<?php Hybrid\Menu\display_name( $data->location ) ?>
-		</h3>
+	<div class="menu__wrapper wrapper">
 		<?php wp_nav_menu( [
 			'theme_location'  => $data->location,
+			'depth'			  => 2,
 			'container'       => '',
 			'container_class' => 'nav-menu',
   			'container_id'    => 'primary-menu-container',
@@ -19,7 +12,7 @@
 			'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
 			'item_spacing'    => 'discard'
 		] ) ?>
-
+		</div>
 	</nav>
 	<a href="#" class="backdrop" tabindex="-1" aria-hidden="true" hidden></a>
 <?php endif ?>
